ab980f293fcb7817a0edd32530cb7c5b12dc4305
Carlo Wood <carlo@alinoe.com>
2025-02-06 17:06:08 +0100

    Move plugin specific debug ostream inserters to aicxx/debug_ostream_operators.h

--- debug_ostream_operators.h
+++ debug_ostream_operators.h
@@ -1,4 +1,5 @@
 #include "cwds/debug_ostream_operators.h"
+#include "aicxx/debug_ostream_operators.h"
 
 #define AI_CASE_RETURN(x) do { case x: return #x; } while(0)
 
@@ -29,39 +30,3 @@ inline std::ostream& operator<<(std::ostream& os, wlr_pointer_button_event const
   os << '}';
   return os;
 }
-
-#include "deco-layout.hpp"
-
-namespace wf::pixdecor {
-
-inline char const* to_string(decoration_layout_action_t action)
-{
-  switch (action)
-  {
-    AI_CASE_RETURN(DECORATION_ACTION_NONE);
-    AI_CASE_RETURN(DECORATION_ACTION_MOVE);
-    AI_CASE_RETURN(DECORATION_ACTION_RESIZE);
-    AI_CASE_RETURN(DECORATION_ACTION_CLOSE);
-    AI_CASE_RETURN(DECORATION_ACTION_TOGGLE_MAXIMIZE);
-    AI_CASE_RETURN(DECORATION_ACTION_MINIMIZE);
-    AI_CASE_RETURN(DECORATION_ACTION_SHADE);
-    AI_CASE_RETURN(DECORATION_ACTION_UNSHADE);
-  }
-  return "Unknown decoration_layout_action_t";
-}
-
-inline std::ostream& operator<<(std::ostream& os, decoration_layout_action_t action)
-{
-  return os << to_string(action);
-}
-
-inline std::ostream& operator<<(std::ostream& os, pixdecor_layout_t::action_response_t const& action_response)
-{
-  os << '{';
-  os << "action:" << action_response.action <<
-      ", edges:" << action_response.edges;
-  os << '}';
-  return os;
-};
-
-} // namespace wf::pixdecor
